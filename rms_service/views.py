4b8a9d76c13a155307e93254d355528a61078699
mohsinzaidiavialdosolutions <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>
2022-08-30 20:52:07 +0500

    initial commit

--- rms_service/views.py
+++ rms_service/views.py
@@ -7,17 +7,24 @@ from django.http import HttpRequest, HttpResponse
 
 
 def config(request: HttpRequest) -> HttpResponse:
-    client = utils.get_restaurant_db_client()
-    db = utils.get_restaurants_database(client)
-    collection = utils.get_restaurants_collection(db)
+    if request.method == "POST":
+        body: dict = json.load(request.body)
+        client = utils.get_restaurant_db_client()
+        db = utils.get_restaurants_database(client)
+        collection = utils.get_restaurants_collection(db)
 
-    cursor = collection.find(projection={"_id": 0, "Branches.Users": 0})
-    data: list = []
-    for c in cursor:
-        data.append(c)
+        cursor = collection.find(
+            {"UniqueId": body["restaurantId"], "Branches.UniqueId": body["branchId"]},
+            projection={"_id": 0, "Branches.Users": 0},
+        )
+        data: list = []
+        for c in cursor:
+            data.append(c)
 
-    cursor.close()
-    client.close()
-    return HttpResponse(
-        json.dumps(data), content_type="application/json", status=HTTPStatus.OK
-    )
+        cursor.close()
+        client.close()
+        return HttpResponse(
+            json.dumps(data), content_type="application/json", status=HTTPStatus.OK
+        )
+    else:
+        return HttpResponse("", status=HTTPStatus.BAD_REQUEST)
