9d443882bde2b466299995c03aeee2a60399dc60
mohsinzaidiavialdosolutions <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>
2022-08-30 21:51:34 +0500

    initial commit

--- rms_service/views.py
+++ rms_service/views.py
@@ -7,24 +7,41 @@ from django.http import HttpRequest, HttpResponse
 
 
 def config(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        body: dict = json.load(request.body)
-        client = utils.get_restaurant_db_client()
-        db = utils.get_restaurants_database(client)
-        collection = utils.get_restaurants_collection(db)
+    error: BaseException
+    try:
+        if request.method == "POST":
+            body: dict = json.loads(request.body)
+            client = utils.get_restaurant_db_client()
+            db = utils.get_restaurants_database(client)
+            collection = utils.get_restaurants_collection(db)
 
-        cursor = collection.find(
-            {"UniqueId": body["restaurantId"], "Branches.UniqueId": body["branchId"]},
-            projection={"_id": 0, "Branches.Users": 0},
-        )
-        data: list = []
-        for c in cursor:
-            data.append(c)
+            cursor = collection.find(
+                {
+                    "UniqueId": body["restaurantId"],
+                    "Branches.UniqueId": body["branchId"],
+                },
+                projection={"_id": 0, "Branches.Users": 0},
+            )
+            data: list = []
+            for c in cursor:
+                document: dict = {
+                    "UniqueId": c["UniqueId"],
+                    "Name": c["Name"],
+                    "Branches": [],
+                }
+                for b in c["Branches"]:
+                    if b["UniqueId"] == body["branchId"]:
+                        document["Branches"].append(b)
+                        # for s in body['systems']:
+                            
 
-        cursor.close()
-        client.close()
-        return HttpResponse(
-            json.dumps(data), content_type="application/json", status=HTTPStatus.OK
-        )
-    else:
-        return HttpResponse("", status=HTTPStatus.BAD_REQUEST)
+                data.append(document)
+
+            cursor.close()
+            client.close()
+            return HttpResponse(
+                json.dumps(data), content_type="application/json", status=HTTPStatus.OK
+            )
+
+    except Exception as e:
+        print(e)
